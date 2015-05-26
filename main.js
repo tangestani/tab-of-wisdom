75b17a1329f06eb4d3969bdd7ea1ea9f99fb9fc4
Mohammed Tangestani <tangestani@gmail.com>
2015-05-26 02:50:31 -0400

    Months start from 0

--- main.js
+++ main.js
@@ -6,49 +6,49 @@ function getUrl() {
     var baseUrl = 'http://a-calendar-of-wisdom.blogspot.ca';
 
     switch (month) {
-        case 1:
+        case 0:
             path = '/2015/01/january-';
             break;
-        case 2:
+        case 1:
             path = '/2015/02/february-';
             break;
-        case 3:
+        case 2:
             path = '/2015/03/march-';
             break;
-        case 4:
+        case 3:
             path = '/2015/04/april-';
             break;
-        case 5:
+        case 4:
             path = '/2012/05/may-';
             break;
-        case 6:
+        case 5:
             if (date < 9)
                 path = '/2012/05/june-';
             else
                 path = '/2012/06/june-';
             break;
-        case 7:
+        case 6:
             if (date < 12)
                 path = '/2012/07/july-';
             else
                 path = '/2012/08/july-';
             break;
-        case 8:
+        case 7:
             path = '/2014/08/august-';
             break;
-        case 9:
+        case 8:
             if (date < 15)
                 path = '/2014/09/september-';
             else
                 path = '/2014/10/september-';
             break;
-        case 10:
+        case 9:
             path = '/2014/10/october-';
             break;
-        case 11:
+        case 10:
             path = '/2014/11/november-';
             break;
-        case 12:
+        case 11:
             path = '/2014/12/december-';
             break;
     }
@@ -56,4 +56,5 @@ function getUrl() {
     return baseUrl + path + date + '.html'
 };
 
+$('#date').text()
 $('#entry').load(getUrl() + ' .entry-content');
